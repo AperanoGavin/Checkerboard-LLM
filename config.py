c43670ddb718422d3e88f1f6bf532b03f3491416
AperanoGavin <audesandrine6@gmail.com>
2024-11-11 21:58:29 +0100

    add new styl and customization

--- config.py
+++ config.py
@@ -8,4 +8,8 @@ CASE_SIZE = SCREEN_HEIGHT // CASES_NUMBER
 # Couleurs
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 import pygame
 from checkerboard import draw_checkerboard
 from config import SCREEN_HEIGHT
+from startmenu import draw_start_menu
 
 # Init of  Pygame
 pygame.init()
+font = pygame.font.Font("AntonSC-Regular.ttf", 50)  
 
 # creat screen
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
 pygame.display.set_caption('checkerboard')
 
 # main loop
-def main():
+''' def main():
     running = True
     while running:
         for event in pygame.event.get():
@@ -22,7 +24,33 @@ def main():
         pygame.display.flip()
 
     # Quit Pygame
-    pygame.quit()
+    pygame.quit() '''
 
+
+def main():
+    running = True
+    in_menu = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if SCREEN_HEIGHT // 3 < mouse_y < SCREEN_HEIGHT // 3 + 50:  # Start Game
+                    in_menu = False
+                elif SCREEN_HEIGHT // 2 < mouse_y < SCREEN_HEIGHT // 2 + 50:  # Custom Button
+                    print("Opening Customization Menu...")
+                    # Ajouter ici la logique pour personnaliser le jeu
+                elif SCREEN_HEIGHT // 1.5 < mouse_y < SCREEN_HEIGHT // 1.5 + 50:  # Quit Button
+                    running = False
+
+        if in_menu:
+            draw_start_menu(screen)
+        else:
+            draw_checkerboard(screen)  # Fonction à définir pour afficher le damier futuriste
+        pygame.display.flip()
+
+    pygame.quit()
 if __name__ == "__main__":
     main()
